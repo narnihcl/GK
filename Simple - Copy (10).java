7d0f94007e85b07118c4991e60a2e4b2e5bb3d4c
narnihcl <narnihcl@gmail.com>
2023-09-04 09:31:33 +0530

    Update Simple - Copy (10).java
    There are few modifications after testing
    Narni

--- Simple - Copy (10).java
+++ Simple - Copy (10).java
@@ -1,3 +1,5 @@
+//This is for modification - 4th Sep 2023
+//Parameters are changed and tested in the local environment
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
